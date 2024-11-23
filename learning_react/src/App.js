9b774e7f28b23975f1baa0019fd2ed30f63f1297
Suryaprakash <kgpsuryaprakash@gmail.com>
2024-11-23 20:15:59 +0530

    Songs changing automatically

--- learning_react/src/App.js
+++ learning_react/src/App.js
@@ -2,22 +2,19 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+const name = "Suryaprakash.G";
+const songs_link = ['https://youtu.be/ubk_MzyPVrY?si=-_yeduAPywsMJjDp' , 'https://youtu.be/TPVT-LWvgZQ?si=MJOjFUNvkhuE2z6V' , 'https://youtu.be/0Z3I8TSUwLI?si=eV9C7flvj-LrGQ2t'];
+const song_name = ['Theansudarae','Aasa Oravae','Hey Minalae'];
+function randomsonglink(){
+  const int = Math.floor(Math.random()*3);
+  return int;
+};
+
+  const random_number = randomsonglink();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <a href={songs_link[random_number]}>Click here to listen song {song_name[random_number]}</a>
+
     </div>
   );
 }
